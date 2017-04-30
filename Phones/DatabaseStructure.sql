fb9be27a80961ed56eba58439f0856df4c7fad99
konstantin <konstantin@linuxkav>
2017-04-30 17:58:09 +0300

    Добавлена структура БД

---
+++ Phones/DatabaseStructure.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.10
+-- https://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Апр 30 2017 г., 14:56
+-- Версия сервера: 5.6.35
+-- Версия PHP: 5.5.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `Phones`
+--
+CREATE DATABASE IF NOT EXISTS `Phones` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `Phones`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Departments`
+--
+
+DROP TABLE IF EXISTS `Departments`;
+CREATE TABLE IF NOT EXISTS `Departments` (
+  `Id` int(11) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Region` int(11) NOT NULL,
+  `IsNotUsed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Departments_contacts`
+--
+
+DROP TABLE IF EXISTS `Departments_contacts`;
+CREATE TABLE IF NOT EXISTS `Departments_contacts` (
+  `department_id` int(5) NOT NULL,
+  `info_type` int(1) NOT NULL,
+  `info_value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Employers`
+--
+
+DROP TABLE IF EXISTS `Employers`;
+CREATE TABLE IF NOT EXISTS `Employers` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `MiddleName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `Department_ID` int(5) NOT NULL,
+  `IsFired` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Employers_contacts`
+--
+
+DROP TABLE IF EXISTS `Employers_contacts`;
+CREATE TABLE IF NOT EXISTS `Employers_contacts` (
+  `employer_ID` int(10) NOT NULL,
+  `contact_type` int(1) NOT NULL,
+  `contact_value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `Regions`
+--
+
+DROP TABLE IF EXISTS `Regions`;
+CREATE TABLE IF NOT EXISTS `Regions` (
+  `ID` int(11) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `IsNotUsed` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `Departments`
+--
+ALTER TABLE `Departments`
+  ADD PRIMARY KEY (`Id`,`Region`);
+
+--
+-- Индексы таблицы `Departments_contacts`
+--
+ALTER TABLE `Departments_contacts`
+  ADD PRIMARY KEY (`department_id`,`info_type`),
+  ADD UNIQUE KEY `department_id` (`department_id`,`info_type`);
+
+--
+-- Индексы таблицы `Employers`
+--
+ALTER TABLE `Employers`
+  ADD PRIMARY KEY (`ID`,`IsFired`,`Department_ID`);
+
+--
+-- Индексы таблицы `Employers_contacts`
+--
+ALTER TABLE `Employers_contacts`
+  ADD PRIMARY KEY (`employer_ID`,`contact_type`);
+
+--
+-- Индексы таблицы `Regions`
+--
+ALTER TABLE `Regions`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `Departments`
+--
+ALTER TABLE `Departments`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `Employers`
+--
+ALTER TABLE `Employers`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `Regions`
+--
+ALTER TABLE `Regions`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- Phones/src/ru/phones/kav/TestEmp.java
+++ Phones/src/ru/phones/kav/TestEmp.java
@@ -11,33 +11,6 @@ public class TestEmp {
 
 	public static void main(String[] args) {
 		//
-		ArrayList<Employee> employers = new ArrayList<>();
-		for(int i = 0; i < 2; i++)
-		{
-			Employee emp;
-			if (i==0)
-			{
-				HashMap<ContactTypes,String> contact = new HashMap<>();
-				contact.put(ContactTypes.EMAIL,"a@a.ru");
-				contact.put(ContactTypes.PHONE,"12345");
-				contact.put(ContactTypes.EXTPHONE,"+7812-12345");
-				emp = new Employee(0,"Василий","Петрович","Пупкин", contact);
-			}
-			else 
-			{
-				HashMap<ContactTypes,String> contact = new HashMap<>();
-				contact.put(ContactTypes.EMAIL,"b@b.ru");
-				contact.put(ContactTypes.PHONE,"3456");
-				contact.put(ContactTypes.EXTPHONE,"+7812-34567");
-				emp = new Employee(0,"Семен","Иванович","Рогов", contact);
-			}
-			employers.add(employers.size(),emp);
-		}
-		
-		for(Employee curEmp : employers){
-			System.out.println(curEmp.toString());
-		}
-		
 		MySQLConnector connector = new MySQLConnector("root", "240981", "127.0.0.1", "Phones");
 		try {
 			if(connector.isConnected())
